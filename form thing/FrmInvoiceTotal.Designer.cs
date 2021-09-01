8d4d4263f6148868e06c12c01a97a06d81952080
Mason Shafer <cg9662vn@go.minnstate.edu>
2021-08-31 19:56:05 -0500

    Did stuffs and things!

--- form thing/FrmInvoiceTotal.Designer.cs
+++ form thing/FrmInvoiceTotal.Designer.cs
@@ -44,9 +44,10 @@ namespace form_thing
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 40);
+            this.label1.Location = new System.Drawing.Point(43, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.Size = new System.Drawing.Size(49, 14);
             this.label1.TabIndex = 0;
             this.label1.Text = "&Subtotal:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -55,9 +56,10 @@ namespace form_thing
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 70);
+            this.label2.Location = new System.Drawing.Point(43, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 15);
+            this.label2.Size = new System.Drawing.Size(94, 14);
             this.label2.TabIndex = 2;
             this.label2.Text = "Discount percemt:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -66,9 +68,10 @@ namespace form_thing
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 100);
+            this.label3.Location = new System.Drawing.Point(43, 93);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.Size = new System.Drawing.Size(90, 14);
             this.label3.TabIndex = 4;
             this.label3.Text = "Discount amount:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -76,72 +79,79 @@ namespace form_thing
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(50, 132);
+            this.label4.Location = new System.Drawing.Point(43, 123);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.Size = new System.Drawing.Size(32, 14);
             this.label4.TabIndex = 6;
             this.label4.Text = "Total:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // txtSubtotal
             // 
-            this.txtSubtotal.Location = new System.Drawing.Point(171, 37);
+            this.txtSubtotal.Location = new System.Drawing.Point(146, 35);
+            this.txtSubtotal.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtSubtotal.Name = "txtSubtotal";
-            this.txtSubtotal.Size = new System.Drawing.Size(199, 23);
+            this.txtSubtotal.Size = new System.Drawing.Size(171, 20);
             this.txtSubtotal.TabIndex = 1;
             // 
             // txtDiscountPercent
             // 
-            this.txtDiscountPercent.Location = new System.Drawing.Point(171, 67);
+            this.txtDiscountPercent.Location = new System.Drawing.Point(146, 63);
+            this.txtDiscountPercent.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtDiscountPercent.Name = "txtDiscountPercent";
-            this.txtDiscountPercent.ReadOnly = true;
-            this.txtDiscountPercent.Size = new System.Drawing.Size(199, 23);
-            this.txtDiscountPercent.TabIndex = 3;
-            this.txtDiscountPercent.TabStop = false;
+            this.txtDiscountPercent.Size = new System.Drawing.Size(171, 20);
+            this.txtDiscountPercent.TabIndex = 2;
+            this.txtDiscountPercent.Text = "5";
             // 
             // txtDiscountAmount
             // 
-            this.txtDiscountAmount.Location = new System.Drawing.Point(171, 97);
+            this.txtDiscountAmount.Location = new System.Drawing.Point(146, 91);
+            this.txtDiscountAmount.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtDiscountAmount.Name = "txtDiscountAmount";
             this.txtDiscountAmount.ReadOnly = true;
-            this.txtDiscountAmount.Size = new System.Drawing.Size(199, 23);
+            this.txtDiscountAmount.Size = new System.Drawing.Size(171, 20);
             this.txtDiscountAmount.TabIndex = 5;
             this.txtDiscountAmount.TabStop = false;
+            this.txtDiscountAmount.TextChanged += new System.EventHandler(this.txtDiscountAmount_TextChanged);
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(171, 128);
+            this.txtTotal.Location = new System.Drawing.Point(146, 119);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
-            this.txtTotal.Size = new System.Drawing.Size(199, 23);
+            this.txtTotal.Size = new System.Drawing.Size(171, 20);
             this.txtTotal.TabIndex = 7;
             this.txtTotal.TabStop = false;
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(128, 177);
+            this.btnCalculate.Location = new System.Drawing.Point(109, 165);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(87, 27);
-            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Size = new System.Drawing.Size(74, 25);
+            this.btnCalculate.TabIndex = 3;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(223, 177);
+            this.btnExit.Location = new System.Drawing.Point(191, 165);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(87, 27);
-            this.btnExit.TabIndex = 3;
+            this.btnExit.Size = new System.Drawing.Size(74, 25);
+            this.btnExit.TabIndex = 4;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 237);
+            this.ClientSize = new System.Drawing.Size(372, 221);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtTotal);
@@ -152,7 +162,8 @@ namespace form_thing
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
             this.Text = "InvoiceTotalForm";
             this.ResumeLayout(false);
