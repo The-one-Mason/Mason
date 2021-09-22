3062e308f48ce2e2e0d9d2e463a1ebefcd8c2287
Mason Shafer <cg9662vn@go.minnstate.edu>
2021-09-21 21:01:24 -0500

    Did 1E Ch2-3 Average Test Score Assignment

---
+++ Average Test Score/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Average_Test_Score
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = ((Convert.ToDecimal(txtTest1.Text) + Convert.ToDecimal(txtTest2.Text) + Convert.ToDecimal(txtTest3.Text))/3).ToString("0.000");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTest1.Text = "0";
+            txtTest2.Text = "0";
+            txtTest3.Text = "0";
+            txtAverage.Text = "0";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
